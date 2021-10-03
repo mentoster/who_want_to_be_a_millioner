c08643a10d0cbf6b2522fa52e7d977058aac1cae
Dmitriy <Dimamakarov1941@yandex.ru>
2021-10-03 21:53:39 +0300

    finish test name page

--- lib/app/ui/pages/test_page/test_page.dart
+++ lib/app/ui/pages/test_page/test_page.dart
@@ -1,20 +1,23 @@
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../../../controllers/test_controller.dart';
 
-
 class TestPage extends GetView<TestController> {
+  final String userName;
+  const TestPage(this.userName, {Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        title: Text('TestPage'),
-      ),
       body: SafeArea(
-        child: Text('TestController'),
+        child: Stack(
+          children: [
+            Image.asset("assets/images/cover.jpg",
+                fit: BoxFit.cover, height: size.height, width: size.width),
+          ],
+        ),
       ),
     );
   }
 }
-  
